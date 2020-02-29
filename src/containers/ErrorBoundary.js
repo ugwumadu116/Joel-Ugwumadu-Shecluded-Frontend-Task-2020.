02fb473af4dde88038bb06b49ed1a2968ea5f708
ugwumadu116 <ugwumadu116@gmail.com>
2020-02-29 23:44:25 +0100

    remove pre-deploy script

--- src/containers/ErrorBoundary.js
+++ src/containers/ErrorBoundary.js
@@ -13,10 +13,6 @@ class ErrorBoundary extends React.Component {
     return { hasError: true };
   }
 
-  componentDidCatch(error, errorInfo) {
-    // You can also log the error to an error reporting service
-    // logErrorToMyService(error, errorInfo);
-  }
 
   render() {
 
